cec9201514ca7faef6600652bef3af067a8ace58
Pawel Nowak <siersciotluk@gmail.com>
2018-12-04 21:31:06 +0100

    format ChangeEntryDao and ChangeEntryIndexDao

--- src/main/java/com/github/mongobeej/dao/ChangeEntryIndexDao.java
+++ src/main/java/com/github/mongobeej/dao/ChangeEntryIndexDao.java
@@ -7,38 +7,38 @@ import org.bson.Document;
 
 public class ChangeEntryIndexDao {
 
-  public void createRequiredUniqueIndex(MongoCollection<Document> collection) {
-    collection.createIndex(new Document()
-            .append(ChangeEntry.KEY_CHANGEID, 1)
-            .append(ChangeEntry.KEY_AUTHOR, 1),
-        new IndexOptions().unique(true)
-    );
-  }
-
-  public Document findRequiredChangeAndAuthorIndex(MongoCollection<Document> collection) {
-    for (Document document : collection.listIndexes()) {
-      Document indexKeys = ((Document) document.get("key"));
-      if (indexKeys != null && isChangelogIndex(indexKeys)) {
-        return document;
-      }
+    public void createRequiredUniqueIndex(MongoCollection<Document> collection) {
+        collection.createIndex(new Document()
+                        .append(ChangeEntry.KEY_CHANGEID, 1)
+                        .append(ChangeEntry.KEY_AUTHOR, 1),
+                new IndexOptions().unique(true)
+        );
     }
-    return null;
-  }
 
-  private boolean isChangelogIndex(Document indexKeys) {
-    return indexKeys.get(ChangeEntry.KEY_CHANGEID) != null && indexKeys.get(ChangeEntry.KEY_AUTHOR) != null;
-  }
-
-  public boolean isUnique(Document index) {
-    Object unique = index.get("unique");
-    if (unique != null && unique instanceof Boolean) {
-      return (Boolean) unique;
-    } else {
-      return false;
+    public Document findRequiredChangeAndAuthorIndex(MongoCollection<Document> collection) {
+        for (Document document : collection.listIndexes()) {
+            Document indexKeys = ((Document) document.get("key"));
+            if (indexKeys != null && isChangelogIndex(indexKeys)) {
+                return document;
+            }
+        }
+        return null;
     }
-  }
 
-  public void dropIndex(MongoCollection<Document> collection, Document index) {
-    collection.dropIndex(index.get("name").toString());
-  }
+    private boolean isChangelogIndex(Document indexKeys) {
+        return indexKeys.get(ChangeEntry.KEY_CHANGEID) != null && indexKeys.get(ChangeEntry.KEY_AUTHOR) != null;
+    }
+
+    public boolean isUnique(Document index) {
+        Object unique = index.get("unique");
+        if (unique != null && unique instanceof Boolean) {
+            return (Boolean) unique;
+        } else {
+            return false;
+        }
+    }
+
+    public void dropIndex(MongoCollection<Document> collection, Document index) {
+        collection.dropIndex(index.get("name").toString());
+    }
 }
